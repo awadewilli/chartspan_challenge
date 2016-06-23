2ec1a5b556f276141ac760198d6bd7dac443730a
Austin Williams <austinwadewilliams@gmail.com>
2016-06-23 11:13:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+$("#submit-btn").click(function(){
+  var ipInt="";
+  var intAddress=0;
+  var ipstring = $('#ip-input').val().split('.');
+  console.log(ipstring);
+  if(ipstring.length>4){
+    $('#int-value').html('invald format');
+  }
+  else{
+    for(var i = 0; i < ipstring.length; i++) {
+      ipInt = parseInt(ipstring[i]) * Math.pow(256,3-i);
+      console.log(ipInt);
+      intAddress = intAddress + ipInt;
+      console.log(intAddress);
+      $('#int-value').html(intAddress);
+    }
+  }
+});
