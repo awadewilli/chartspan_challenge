5d5628b2d6cb1156453b498ee8d9212448de77ed
Austin Williams <austinwadewilliams@gmail.com>
2016-06-23 16:08:22 -0400

    added address validation

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ $("#submit-btn").click(function(){
   var intAddress=0;
   var ipstring = $('#ip-input').val().split('.');
   console.log(ipstring);
-  if(ipstring.length>4){
-    $('#int-value').html('invald format');
-  }
-  else if (ipstring<4) {
-    $('#int-value').html('invalid format');
+  if(ipstring.length>4||ipstring.length<4){
+    $('#int-value').html('invalid address');
   }
   else{
     for(var i = 0; i < ipstring.length; i++) {
-      ipInt = parseInt(ipstring[i]) * Math.pow(256,3-i);
-      console.log(ipInt);
-      intAddress = intAddress + ipInt;
-      console.log(intAddress);
-      $('#int-value').html(intAddress);
+      if(ipstring[i]>255 || ipstring[i]<0 || isNaN(ipstring[i])){
+        $('#int-value').html('invalid address');
+        break;
+      }
+      else{
+        ipInt = parseInt(ipstring[i]) * Math.pow(256,3-i);
+        console.log(ipInt);
+        intAddress = intAddress + ipInt;
+        console.log(intAddress);
+        $('#int-value').html(intAddress);
+      }
     }
   }
 });
